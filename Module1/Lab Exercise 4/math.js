76666cc0344b84cd244a9396dc1305eb25bbe025
FullMetal0508 <joshuafuimaono@hotmail.com>
2024-08-03 16:11:19 +1200

    maths

---
+++ Module1/Lab Exercise 4/math.js
@@ -0,0 +1,24 @@
+function sum(a, b)
+{
+    return(a+b)
+}
+
+function subtract(a, b)
+{
+    return(a-b)
+}
+
+function multiply(a, b)
+{
+    return(a*b)
+}
+
+function divide(a, b)
+{
+    return(a/b)
+}
+
+console.log(sum(3, 4))
+console.log(subtract(10, 5))
+console.log(multiply(10,5))
+console.log(divide(10,5))
