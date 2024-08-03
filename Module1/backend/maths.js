55072238986bb858d9354f59e375f0053dec24cb
FullMetal0508 <joshuafuimaono@hotmail.com>
2024-08-03 15:38:54 +1200

    recreated filling system

---
+++ Module1/backend/maths.js
@@ -0,0 +1,21 @@
+function Add(a,b)
+{
+    return a + b
+}
+
+function Subtract(a,b)
+{
+   return a - b
+}
+
+function Multiply(a,b)
+{
+    return a * b
+}
+
+function Divide(a,b)
+{
+    return a / b
+}
+
+console.log Divide(12,3)
