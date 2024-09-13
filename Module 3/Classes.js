45647e9e3b7c48bd209fa6fd037b2bee14f4ebc6
FullMetal0508 <joshuafuimaono@hotmail.com>
2024-09-14 09:24:02 +1200

    q4

---
+++ Module 3/Classes.js
@@ -0,0 +1,10 @@
+class DnDClass (STR, DEX, CON, WIS, INT, CHA)
+{
+    this.STR = Stength
+    this.DEX = Dexterity
+    this.CON = Constitution
+    this.WIS = Wisdom
+    this.INT = Intelligence
+    this.CHA = Charisma
+    
+}
