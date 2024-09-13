a5cfade0c84131cdd76c1369d8020e0d6a3f7868
FullMetal0508 <joshuafuimaono@hotmail.com>
2024-09-13 18:20:14 +1200

    Mod3LabExBegin

---
+++ Module 3/LabExJSFundamentals/Question1.js
@@ -0,0 +1,47 @@
+// 1.1 = 10
+console.log("" + 1 + 0)
+
+//1.2 = -1
+console.log("" - 1 + 0)
+
+//1.3 = 1
+console.log (true + false)
+
+//1.4 = false
+console.log(!true)
+
+//1.5 = 2
+console.log(6 / "3")
+
+//1.6 = 6
+console.log ("2" * "3")
+
+//1.7 = 9px
+console.log(4 + 5 + "px")
+
+//1.8 = $45
+console.log("$" + 4 + 5)
+
+//1.9 = NaN
+console.log("4px" - 2)
+
+//1.10 = -95
+console.log("-9" + 5)
+
+//1.11 =  -14
+console.log("-9" - 5)
+
+//1.12 = 1
+console.log(null + 1)
+
+//1.13 = NaN
+console.log(undefined + 1)
+
+//1.14 = true
+console.log(undefined == null)
+
+//1.15 = false
+console.log(undefined === null)
+
+//1.16 = - 2
+console.log(" \t \n" -2)
---
+++ Module 3/LabExJSFundamentals/Question3.js
@@ -0,0 +1,8 @@
+if (0) console.log('#1 zero is true')
+if ("0") console.log('#2 zero is true')
+if (null) console.log('null is true')
+if (-1) console.log('negative is true')
+if (1) console.log('positive is true')
+
+//honestly dunno why they do or don't work. Will have to check with Mirza.
+//is it because null and integer 0 equals nothing??
