e41fe9ecac01479fb0b48f7c966951b27ee51b61
FullMetal0508 <joshuafuimaono@hotmail.com>
2024-09-18 19:25:44 +1200

    Mod3LabFundQ6

--- Module 3/LabExJSFundamentals/Question6.js
+++ Module 3/LabExJSFundamentals/Question6.js
@@ -10,14 +10,20 @@ const rugen = {
 
 const inigo = {
   firstName: 'Inigo',
+  lastName: 'Montoya',
   greeting(person) {
-  let greeting = `Hello ${person.name}, my name is ${this.firstName}. `;
+  let greeting = `Hello ${person.name}, my name is ${this.firstName} ${this.lastName}. `;
   console.log(greeting + this.getCatchPhrase(person));
   },
   getCatchPhrase(person) {
-  return 'Nice to meet you.';
+    if (person.numFingers == 6) {
+      return 'You killed my father. Prepare to die.'
+    } 
+    else {
+      return 'Nice to meet you.'
+    }
   }
-  }
+}
 
 inigo.greeting(westley);
 inigo.greeting(rugen);
