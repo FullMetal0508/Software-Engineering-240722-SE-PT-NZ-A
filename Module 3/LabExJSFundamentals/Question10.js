5618dcc331d0fead25500ad1f7881de0399090b5
FullMetal0508 <joshuafuimaono@hotmail.com>
2024-09-19 22:48:57 +1200

    Mod3LabFundQ10

---
+++ Module 3/LabExJSFundamentals/Question10.js
@@ -0,0 +1,7 @@
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.human = true;
+}
+
+let person1 = new Person(Josh, 30)
---
+++ Module 3/LabExJSFundamentals/Question9.js
@@ -0,0 +1,20 @@
+let teamSports = ['Hockey', 'Cricket', 'Volleyball'];
+let moreSports = teamSports
+
+moreSports.push('Rugby')
+moreSports.push('Football')
+
+console.log(teamSports)
+console.log(moreSports)
+
+
+let dog1 = 'Bingo';
+let dog2 = dog1;
+
+
+console.log(dog1)
+
+let cat1 = { name: 'Fluffy', breed: 'Siberian' };
+let cat2 = {...cat1, legs: 4, tail: 'yes'}
+
+console.log(cat2)
