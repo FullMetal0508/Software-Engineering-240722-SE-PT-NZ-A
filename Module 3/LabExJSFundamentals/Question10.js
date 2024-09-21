4252d5bd0ed6ecced956dadea45583873ebfe84b
FullMetal0508 <joshuafuimaono@hotmail.com>
2024-09-22 11:45:33 +1200

    Mod3LabFundEnd

--- Module 3/LabExJSFundamentals/Question10.js
+++ Module 3/LabExJSFundamentals/Question10.js
@@ -1,7 +1,24 @@
-function Person(name, age) {
+function Person (name, age){
   this.name = name;
   this.age = age;
   this.human = true;
-}
+  this.canDrive = this.age >= 16;
+  }
+  
 
-let person1 = new Person(Josh, 30)
+let person1 = new Person("Josh", 12)
+
+console.log(person1)
+
+function PersonClass(name, age) {
+
+  this.name = name;
+  this.age = age;
+  this.human = true;
+  this.canDrive = this.age >= 16;
+
+  }
+
+let person2 = new PersonClass("Sarai", 30)
+
+console.log(person2)
