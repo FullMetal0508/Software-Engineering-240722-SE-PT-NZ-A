2b4f54b53a8374273f609eac6c2a94b2b5180979
FullMetal0508 <joshuafuimaono@hotmail.com>
2024-09-23 18:28:53 +1200

    Mod4Figma

--- Module 3/LabExJSIntermediate/Question10.js
+++ Module 3/LabExJSIntermediate/Question10.js
@@ -1,5 +1,10 @@
 const today = new Date();
+let todaySeconds = new Date(1000*60*60*24)
+
 
 console.log("Current time is " + today.toLocaleTimeString());
 console.log(today.getHours() + " hours have passed so far today");
 console.log(today.getSeconds() + " seconds have passed so far today");
+console.log(todaySeconds + " seconds have passed so far today")
+
+
