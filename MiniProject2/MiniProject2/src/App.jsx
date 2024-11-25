db70e2866cf19093967f193b8fc3d1e47d03c815
FullMetal0508 <joshuafuimaono@hotmail.com>
2024-11-25 17:15:58 +1300

    MiniProject2WIP

---
+++ MiniProject2/MiniProject2/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import DisplayPrimarchs from './Components/DisplayPrimarchs'
+
+
+function App() {
+
+  return (
+    <>
+    
+    <DisplayPrimarchs></DisplayPrimarchs>
+   
+     
+    </>
+  )
+}
+
+export default App
